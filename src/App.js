5193047aa9a3c6f596d8c2fa3dcaad96f9cbf78f
logan.young <logan.young@autoprotectgroup.co.uk>
2023-02-23 11:26:41 +0000

    Basic Shit Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import sls from './sls.png';
+import fdm from './fdm.png';
+import sla from './sla.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>3D Printing</h1>
+        <h3>and the overly abitious things that follow</h3>
+        <div className="Images">
+          <img src={fdm} alt="fdm" />
+          <img src={sls} alt="sls" />
+          <img src={sla} alt="sla" />
+        </div>
     </div>
   );
 }
